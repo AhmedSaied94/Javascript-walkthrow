cf33bea64078dd1120ac2e85aaf1b0dc18a58b13
AhmedSaied94 <ahmed.saeed311294@gmail.com>
2021-08-27 14:49:44 +0200

    first change

---
+++ test.py
@@ -0,0 +1,21 @@
+import re
+"""
+n = int(input())
+if n%2 != 0:
+    print("Weird")
+elif n%2 == 0 and n in [2, 3, 4, 5]:
+    print("Not Weird") 
+elif n%2 == 0 and n in range(21):
+    print("Weird") 
+elif n%2 == 0 and n >= 21:
+    print("Not Weird") 
+"""   
+n = "2 9587456281 1252478965"
+N = re.findall(r"(?:^|\b)([0-9]+)(?:\b)",n)   
+for x in N:
+    if len(x) == 10:
+      if x[0] in ["7", "8", "9"]:
+        print("Yes")
+      else: 
+        print("No")
+
